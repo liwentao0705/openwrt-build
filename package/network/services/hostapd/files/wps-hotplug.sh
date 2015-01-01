7a7c2aa4d2c4321d48af1fce5e78b2379d48a1ed
liwentao <liwentao0705@outlook.com>
2015-01-01 13:13:18 +0800

    按照981213的提示修改

--- package/network/services/hostapd/files/wps-hotplug.sh
+++ package/network/services/hostapd/files/wps-hotplug.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 . /lib/functions/leds.sh
 
-WPS_LED=$(ls /sys/class/leds | grep -E 'qss|wps' | awk '{printf $1}')
+WPS_LED=$(ls /sys/class/leds | grep -E 'qss|wps' | head -n 1)
 
 if [ "$ACTION" = "pressed" -a "$BUTTON" = "wps" ]; then
 	[ -z $WPS_LED ] || led_timer $WPS_LED 150 150
